cdb005cfa6d0a89ed424cb42641322ef3dd3f357
Theo Vora <theo.vora@gmail.com>
2024-09-29 14:30:20 -0500

    display input unit as a segmented Picker

--- 01*challenge-UnitConversion/Unit Conversion/Unit Conversion/ContentView.swift
+++ 01*challenge-UnitConversion/Unit Conversion/Unit Conversion/ContentView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @State var inputValue: Double = 0.0
+    @State var inputUnit: String = "milliliters"
+    
+    var volumeUnits = [
+        "milliliters",
+        "liters",
+        "cups",
+        "pints",
+        "gallons"
+    ]
 
     var body: some View {
         NavigationStack {
             Form {
                 Section("Input Value") {
                     TextField("Input Value", value: $inputValue, format: .number)
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(volumeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
             }
             .navigationTitle("Volume Conversion")
