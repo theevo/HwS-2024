1f16f24d56b88e09a97243f57b201fcc53cf9798
Theo Vora <theo.vora@gmail.com>
2024-09-29 14:34:10 -0500

    display output Value as Text, output unit as segmented Picker

--- 01*challenge-UnitConversion/Unit Conversion/Unit Conversion/ContentView.swift
+++ 01*challenge-UnitConversion/Unit Conversion/Unit Conversion/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State var inputValue: Double = 0.0
     @State var inputUnit: String = "milliliters"
+    @State var outputUnit: String = "gallons"
+    
+    var outputValue: Double = 500
     
     var volumeUnits = [
         "milliliters",
@@ -25,6 +28,15 @@ struct ContentView: View {
                 Section("Input Value") {
                     TextField("Input Value", value: $inputValue, format: .number)
                     Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(volumeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Output Value") {
+                    Text("\(outputValue)")
+                    Picker("Output Unit", selection: $outputUnit) {
                         ForEach(volumeUnits, id: \.self) {
                             Text($0)
                         }
